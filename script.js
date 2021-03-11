9a09a105a85a9402fd7166c93934806c2ad3514d
Rayhan Mursalin <rmursalin@webmd.net>
2021-03-10 23:32:25 -0500

    Update clock

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ const timer = {
 const modeButtons = document.querySelector('#js-mode-buttons');
 modeButtons.addEventListener('click', handleMode);
 
+function updateClock() {
+	const { remainingTime } = timer;
+	const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+	const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+  
+	const min = document.getElementById('js-minutes');
+	const sec = document.getElementById('js-seconds');
+	min.textContent = minutes;
+	sec.textContent = seconds;
+  }
+  
+
 function switchMode(mode) {
 	timer.mode = mode;
 	timer.remainingTime = {
